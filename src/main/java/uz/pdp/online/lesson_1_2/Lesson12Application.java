b09500e332e5dd0b15ca6768d8a44439ed4ef4a8
Abbosxon <abbosxon28012004@gmail.com>
2022-05-08 21:41:33 +0500

    nullCase

---
+++ src/main/java/uz/pdp/online/lesson_1_2/Lesson12Application.java
@@ -0,0 +1,13 @@
+package uz.pdp.online.lesson_1_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson12Application.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/online/lesson_1_2/Lesson12ApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.online.lesson_1_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson12ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
